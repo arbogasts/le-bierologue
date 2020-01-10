61488a0678e8f203ac23bd8b79b6f13a49f0dfd4
Sébastien ARBOGAST <sarbogast@oci.fr>
2020-01-10 16:09:04 +0100

    Set tabs of beer info

--- src/components/Beer/BeerInfo.js
+++ src/components/Beer/BeerInfo.js
@@ -4,7 +4,7 @@ import Loading from "../Loading";
 import Card from "react-bootstrap/Card";
 import Container from 'react-bootstrap/Container'
 import ListGroup from 'react-bootstrap/ListGroup'
-import { Row, Col, Badge, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Row, Col, Badge, OverlayTrigger, Tooltip, Tabs, Tab } from "react-bootstrap";
 
 const BeerInfo = props => {
   // on récupère la fonction dispatch qui permettra de dispatcher des actions
@@ -47,48 +47,71 @@ const BeerInfo = props => {
 
   return (
 	<div className="beer-details">
-		<Card>
-			<Card.Body>
-				<Container>
-					<Row>
-						<Col sm={4} xs={12}>
-							{
-								beer.labels ? 
-								<img alt="" src={beer.labels.medium} className="d-inline-block align-top"/>
-								: <img alt="Icône par défaut des bières" src="../img/default.png" className="d-inline-block align-top"/>
-							}
-						</Col>
-						<Col sm={8} xs={12} className="mt-4">
-							<Card.Title>{beer.nameDisplay || <Loading/>}</Card.Title>
-							<p title="Style de la bière">{
-								beer.style ?
-								beer.style.category.name 
-								: null
+		<Container>
+			<Card>
+				<Card.Body>
+					<Container>
+						<Row>
+							<Col sm={4} xs={12}>
+								{
+									beer.labels ? 
+									<img alt="" src={beer.labels.medium} className="d-inline-block align-top"/>
+									: <img alt="Icône par défaut des bières" src="../img/default.png" className="d-inline-block align-top"/>
 								}
+							</Col>
+							<Col sm={8} xs={12} className="mt-4">
+								<Card.Title>{beer.nameDisplay || <Loading/>}</Card.Title>
+								<p title="Style de la bière">{
+									beer.style ?
+									beer.style.category.name 
+									: null
+									}
+								</p>
+								<ListGroup horizontal className='justify-content-center'>
+									<ListGroup.Item>{beer.abv}°</ListGroup.Item>
+									<ListGroup.Item>{beer.ibu} IBU 
+										<OverlayTrigger overlay={
+											<Tooltip id="tooltip">
+												La valeur en IBU (International Bitterness Unit) va donner une idée de l'amertume de la bière. Plus le nombre est élevé, plus la bière sera amère.
+											</Tooltip>
+										}>
+											<Badge variant="info">?</Badge>
+										</OverlayTrigger> 
+									</ListGroup.Item>
+								</ListGroup>
+							</Col>
+							<Col sm={12} md={12}>
+								<Card.Text>
+									<p>Description de la bière : </p>
+									{beer.description}
+								</Card.Text>
+							</Col>
+						</Row>
+					</Container>
+				</Card.Body>
+			</Card>
+		</Container>
+		<Container>
+			<Card>
+				<Card.Body>
+					<Tabs defaultActiveKey="glasses" id="tab-details-beer">
+						<Tab eventKey="glasses" title="Dans quelle verre ?">
+							{
+								beer.glass ?
+								<img alt="Image du verre de bière associé" src={`/img/glasses/${beer.glass.name}.jpg`}/>
+								: (<p>Aucunes informations disponibles</p>)
+							}
+						</Tab>
+						<Tab eventKey="foods" title="Que manger avec ?">
+							<p>
+								{beer.foodPairings || "Aucunes informations disponibles"}
 							</p>
-							<ListGroup horizontal className='justify-content-center'>
-								<ListGroup.Item>{beer.abv}°</ListGroup.Item>
-								<ListGroup.Item>{beer.ibu} IBU 
-									<OverlayTrigger overlay={
-										<Tooltip id="tooltip">
-											La valeur en IBU (International Bitterness Unit) va donner une idée de l'amertume de la bière. Plus le nombre est élevé, plus la bière sera amère.
-										</Tooltip>
-									}>
-										<Badge variant="info">?</Badge>
-									</OverlayTrigger> 
-								</ListGroup.Item>
-							</ListGroup>
-						</Col>
-						<Col sm={12} md={12}>
-							<Card.Text>
-								<p>Description de la bière : </p>
-								{beer.description}
-							</Card.Text>
-						</Col>
-					</Row>
-				</Container>
-			</Card.Body>
-		</Card>
+						</Tab>
+					</Tabs>
+				</Card.Body>
+			</Card>
+			
+		</Container>
 	</div>
   );
 };
