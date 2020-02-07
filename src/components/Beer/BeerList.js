76b17ff213c36fa166238cc7bf51cde45381ee39
Sébastien ARBOGAST <sarbogast@oci.fr>
2020-02-07 10:57:27 +0100

    Ajout pagination axios load more

--- src/components/Beer/BeerList.js
+++ src/components/Beer/BeerList.js
@@ -3,19 +3,20 @@ import axios from "axios";
 import Loading from "../Loading";
 
 import Beer from "./Beer";
+import Button from "react-bootstrap/Button";
 
 const BeerList = props => {
   // useSelector permet d'accéder à une valeur enregistrée dans le store redux
   // const [beerList, setBeerList] = useSelector(reduxState => reduxState.beerReducer.list);
-  const loading = useState(false);
   const [beerList, setBeerList] = useState([]);
+  const [page, setPage] = useState(1);
   const key = process.env.REACT_APP_BREWERY_SECRET;
 
-  useEffect(() => {
-    axios.get("/beers?key=" + key)
+  function fetchBeer(){
+    axios.get("/beers?key=" + key + '&p=' + page)
     .then(response => {
-        console.log(response.data);
-        setBeerList(response.data.data);
+        console.log(response.data.data);
+        setBeerList(beerList.concat(response.data.data));
       })
     .catch(function (error) {
       if (error.response) {
@@ -29,7 +30,18 @@ const BeerList = props => {
       }
     console.log(error.config);
     });
-  }, []);
+  }
+
+  const handleMore = event => {
+    event.preventDefault();
+    // Traitement avec chargement supplémentaire
+    let newPage = page + 1;
+    setPage(newPage);
+  }
+
+  useEffect(() => {
+    fetchBeer();
+  }, [page]);
 
   return (
     <div>
@@ -38,6 +50,11 @@ const BeerList = props => {
         {beerList.map(beer => {
           return <Beer key={beer.id} id={beer.id} icon={beer.labels || "./img/default.png"} nameDisplay={beer.nameDisplay}/>;
         })}
+        <div className="text-center">
+          <Button variant="warning" key="load-more" id="load-more" onClick={handleMore}>
+            Charger plus
+          </Button>
+        </div>
       </div>
     </div>
   );
