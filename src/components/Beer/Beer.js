905234bbd60fd8c0bb219103ef04d05f65502d8c
Sébastien ARBOGAST <sarbogast@oci.fr>
2020-02-07 08:37:31 +0100

    Ajout delete Beer

--- src/components/Beer/Beer.js
+++ src/components/Beer/Beer.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 
 const Beer = props => {
   // on récupère la fonction dispatch qui permettra de dispatcher des actions
+  const [idBeer, setIdBeer] = useState('');
+  const key = process.env.REACT_APP_BREWERY_SECRET;
+
+  const handleDelete = event => {
+    event.preventDefault();
+    // Event de confirmation
+
+    // Traitement de la suppression
+    axios.delete(`/beer/${idBeer}?key=${key}`,
+      {
+          headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          },
+      })
+    .then(function (response) {
+        // handle success
+        console.log(response);
+        if(response.status === 201){
+          console.log('success');
+        }
+        else {
+            // setError(true)
+        }
+    })
+    .catch(function (error) {
+        // handle error
+        console.error(error);
+        // setError(true);
+    });
+  }
+
+  // Chargement de l'ID dans notre objet pour le delete
+  useEffect(() => {
+    setIdBeer(props.id)
+  }, [props.id])
 
   return (
-    <Link to={`/beer/${props.id}`} className="card-link">
+    <Link to={`/beer/${idBeer}`} className="card-link">
       <Card style={{ width: '18rem' }} className="card-beer">
+        <Button onClick={handleDelete} variant="danger">Delete</Button>
         <Card.Img className="img-beer" variant="top" src={props.icon.medium || props.icon} />
         <Card.Body>
           <Card.Title className="text-warning">{props.nameDisplay}</Card.Title>
